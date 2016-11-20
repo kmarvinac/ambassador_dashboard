e4888698187c614fd0b43086e082765775c341bd
Raghu Betina <raghu@firstdraft.com>
2016-11-20 01:01:40 +0000

    Whitelist columns in School for ActiveAdmin

--- app/admin/school.rb
+++ app/admin/school.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register School do
 
+ permit_params :school_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
