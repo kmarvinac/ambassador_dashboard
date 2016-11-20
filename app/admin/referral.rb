85fa659185aec75bed7610f20d8145b220cf041b
Raghu Betina <raghu@firstdraft.com>
2016-11-20 01:01:44 +0000

    Whitelist columns in Referral for ActiveAdmin

--- app/admin/referral.rb
+++ app/admin/referral.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Referral do
 
+ permit_params :referrer_id, :school_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
