8001a1cb4bf471ad2c939f076a48211b774acfd6
Neel Bhat <neelbhat88@gmail.com>
2017-02-06 10:07:53 -0500

    Updating API endpoint urls and adding call to get users count by school

--- app/models/transparent_career.rb
+++ app/models/transparent_career.rb
@@ -18,7 +18,7 @@ class TransparentCareer
 
   def user_referrals(user_id)
     query = {query: {accepted: true}}
-    response = self.class.get("/users/#{user_id}/referrals", @headers.merge(query))
+    response = self.class.get("/ambassadors/#{user_id}/referrals", @headers.merge(query))
     if response.success?
       object_array = JSON.parse response.body, symbolize_names: true
       return object_array.map{|o| OpenStruct.new(o)}
@@ -29,7 +29,22 @@ class TransparentCareer
 
   def all_referrals(user_ids)
     query = {query: {user_ids: user_ids, accepted: true}}
-    response = self.class.post("/referrals", @headers.merge(query))
+    response = self.class.post("/ambassadors/referrals", @headers.merge(query))
+    if response.success?
+      return_object = JSON.parse response.body, symbolize_names: true
+      return return_object.map{|o| OpenStruct.new(o)}
+    else
+      return nil
+    end
+  end
+
+  def users_count_by_schools(args)
+    school_ids = args[:school_ids]
+    class_year_start = args[:class_year_start] || Time.zone.now.year.to_s
+    class_year_end = args[:class_year_end] || (Time.zone.now + 4.years).year.to_s
+
+    query = {query: {school_ids: school_ids, class_year_start: class_year_start, class_year_end: class_year_end}}
+    response = self.class.post("/ambassadors/referrals/schools", @headers.merge(query))
     if response.success?
       return_object = JSON.parse response.body, symbolize_names: true
       return return_object.map{|o| OpenStruct.new(o)}
